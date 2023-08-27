0252d5a92b417cb26ced59d49e8f427e1e9ea2d1
Avantika87 <ag8498@srmist.edu.in>
2023-08-27 18:39:39 +0530

    Added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import AttendanceTable from './AttendanceTable';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='heading'><h1>ATTENDANCE</h1></div>
+        <AttendanceTable />
+      </div>
+  );
+}
+
+export default App;
